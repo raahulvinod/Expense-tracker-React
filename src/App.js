b68a38a3c52355b22fb015ca6d117ace2fddc8c6
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-05-03 15:42:34 +0530

    Organizing component files

--- src/App.js
+++ src/App.js
@@ -1,43 +1,7 @@
-import ExpenseItem from './components/ExpenseItem';
 import './App.css';
-
+import Expenses from './components/Expenses/Expenses';
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Fuel',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
-  return (
-    <div className="App">
-      {expenses.map((expense) => {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        );
-      })}
-    </div>
-  );
+  return <Expenses />;
 }
 
 export default App;
